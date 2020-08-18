26cec47f048a4953106a201d4390def7c608f35f
Davi3684 <45174060+DavidHulstroem@users.noreply.github.com>
2020-08-18 19:20:13 +0200

    1

---
+++ BossModules/BossWeaponClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using UnityEngine;
+
+namespace BossModules
+{
+    public class BossWeaponClass : MonoBehaviour
+    {
+        public bool playerHolding;
+        public bool isHeld;
+
+        public virtual void Initialize(Item item)
+        {
+            item.OnGrabEvent += Item_OnGrabEvent;
+            item.OnUngrabEvent += Item_OnUngrabEvent;
+        }
+
+        private void Item_OnUngrabEvent(Handle handle, Interactor interactor, bool throwing)
+        {
+            isHeld = false;
+            playerHolding = false;
+        }
+
+        private void Item_OnGrabEvent(Handle handle, Interactor interactor)
+        {
+            if (interactor.bodyHand.body.creature == Creature.player)
+            {
+                playerHolding = true;
+            } else
+            {
+                playerHolding = false;
+            }
+        }
+    }
+}
