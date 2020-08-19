ccce0ead8d019f91e999ef534a5cdeb1fc5699fa
Davi3684 <45174060+DavidHulstroem@users.noreply.github.com>
2020-08-19 20:18:23 +0200

    Work

--- BossModules/BossWeaponClass.cs
+++ BossModules/BossWeaponClass.cs
@@ -10,19 +10,72 @@ namespace BossModules
 {
     public class BossWeaponClass : MonoBehaviour
     {
+        public delegate void OnUngrab(Handle handle, Interactor interactor, bool throwing);
+        public static event OnUngrab OnUngrabEvent;
+
+        public delegate void OnGrab(Handle handle, Interactor interactor);
+        public static event OnGrab OnGrabPlayerEvent;
+        public static event OnGrab OnGrabEnemyEvent;
+
+
+        public delegate void OnCollision(ref CollisionStruct collisionInstance);
+        public static event OnCollision OnCollisionPlayerHoldingEvent;
+        public static event OnCollision OnCollisionEnemyHoldingEvent;
+        public static event OnCollision OnCollisionNooneHolding;
+
         public bool playerHolding;
         public bool isHeld;
+
+        public Item item;
 
         public virtual void Initialize(Item item)
         {
             item.OnGrabEvent += Item_OnGrabEvent;
             item.OnUngrabEvent += Item_OnUngrabEvent;
+
+            foreach (ColliderGroup colliderGroup in item.definition.colliderGroups)
+            {
+                colliderGroup.collisionHandler.OnCollisionStartEvent += CollisionHandler_OnCollisionStartEvent;
+            }
+            this.item = item;
+        }
+
+        private void CollisionHandler_OnCollisionStartEvent(ref CollisionStruct collisionInstance)
+        {
+            if (isHeld)
+            {
+                if (playerHolding)
+                {
+                    if (OnCollisionPlayerHoldingEvent != null)
+                    {
+                        OnCollisionPlayerHoldingEvent(ref collisionInstance);
+                    }
+                } else
+                {
+                    if (OnCollisionEnemyHoldingEvent != null)
+                    {
+                        OnCollisionEnemyHoldingEvent(ref collisionInstance);
+                    }
+                }
+            } else
+            {
+                if (OnCollisionNooneHolding != null)
+                {
+                    OnCollisionNooneHolding(ref collisionInstance);
+                }
+            }
+            
         }
 
         private void Item_OnUngrabEvent(Handle handle, Interactor interactor, bool throwing)
         {
             isHeld = false;
             playerHolding = false;
+            if (OnUngrabEvent != null)
+            {
+                OnUngrabEvent(handle, interactor, throwing);
+            }
+            
         }
 
         private void Item_OnGrabEvent(Handle handle, Interactor interactor)
@@ -30,9 +83,19 @@ namespace BossModules
             if (interactor.bodyHand.body.creature == Creature.player)
             {
                 playerHolding = true;
+                if (OnGrabPlayerEvent != null)
+                {
+                    OnGrabPlayerEvent(handle, interactor);
+                }
+                
             } else
             {
                 playerHolding = false;
+
+                if (OnGrabEnemyEvent != null)
+                {
+                    OnGrabEnemyEvent(handle, interactor);
+                }
             }
         }
     }
---
+++ BossModules/WeaponAutoScaler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using UnityEngine;
+
+namespace BossModules
+{
+    class WeaponAutoScalerModule : ItemModule
+    {
+        public override void OnItemLoaded(Item item)
+        {
+            base.OnItemLoaded(item);
+            item.gameObject.AddComponent<WeaponAutoScaler>().Initialize(item);
+        }
+    }
+
+    class WeaponAutoScaler : BossWeaponClass
+    {
+
+        public override void Initialize(Item item)
+        {
+            base.Initialize(item);
+            OnGrabPlayerEvent += ItemGrabbedEvent;
+            OnGrabEnemyEvent += ItemGrabbedEvent;
+        }
+
+        private void ItemGrabbedEvent(Handle handle, Interactor interactor)
+        {
+            if (interactor.bodyHand.body.creature.transform.localScale != Vector3.one)
+            {
+                item.transform.localScale = interactor.bodyHand.body.creature.transform.localScale;
+            }
+        }
+    }
+}
